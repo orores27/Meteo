b171420566865db0d470535bba86a47e2d174725
orores27 <sabah.errifi@gmail.com>
2022-06-01 16:00:58 +0200

    API finie

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 //Clé d'authentification pour se connecter à l'API
-const APIKEY = "9ea212ed9540d19e87803ef4d385a4cb";
+const APIKEY = "7303129fa7860152545e0931db16cad4";
 
 //On crée la variable dans laquelle on va stocker nos résultats météo
 let apiResults;
@@ -37,39 +37,93 @@ function callAPI(lat, long) {
         apiResults = data;
         console.log(apiResults);
 
+        const temperatureActuelle = document.querySelector('.temperature');
+        temperatureActuelle.textContent = apiResults.current.temp;
+
+        const humiditeActuelle = document.querySelector('.humidite');
+        humiditeActuelle.textContent = apiResults.current.humidity;
+
+        const pressionActuelle = document.querySelector('.pression');
+        pressionActuelle.textContent = apiResults.current.pressure;
+
+        const vitesseActuelle = document.querySelector('.vitesse');
+        vitesseActuelle.textContent = apiResults.current.wind_speed;
+
+        const descriptionMeteo = document.querySelector('.descriptionTemps');
+        descriptionMeteo.textContent = apiResults.current.weather[0].description;
+
+        // 1) on selectionne l'emplacement pour l'heure 2) on déclare une variable date avec *1000 sert à convertir en milliseconde 3)donner le contenu à afficher
+        const heureActuelle = document.querySelector('.heure');
+        var date = new Date(apiResults.current.dt * 1000);
+        heureActuelle.textContent = date.toLocaleTimeString();
+
+        const cartes = document.querySelectorAll('.card');
+        for(i=0; i<cartes.length; i++){
+            
+            const temperature = cartes[i].querySelector('li:nth-child(1)');
+            temperature.textContent = apiResults.daily[i].temp.day + ' °C';
+
+            const humidite = cartes[i].querySelector('li:nth-child(2)')
+            humidite.textContent = apiResults.daily[i].humidity + ' %';
+
+            const heure = cartes[i].querySelector('li:nth-child(3)')
+            var date = new Date(apiResults.daily[i].dt * 1000);
+            heure.textContent = date.toLocaleTimeString();
+
+            const pression = cartes[i].querySelector('li:nth-child(4)')
+            pression.textContent = apiResults.daily[i].pressure + ' aPh';
+
+            const vitesse = cartes[i].querySelector('li:nth-child(5)')
+            vitesse.textContent = apiResults.daily[i].wind_speed + ' Km/h';
+
+            const descriptionMeteo = cartes[i].querySelector('p');
+            descriptionMeteo.textContent = apiResults.daily[i].weather[0].description;
+
+
+            const imageMeteo = cartes[i].querySelector('img');
+            imageMeteo.src = `./assets/img/day/${apiResults.daily[i].weather[0].icon}.svg`;
+
+            let dayDate = new Date(apiResults.daily[i].dt * 1000).toLocaleString('fr-FR', { weekday: 'long' });
+
+
+            const titreCarte = cartes[i].querySelector('.card-title');
+            titreCarte.textContent = dayDate;
+
+        }
+
         //On utilise maintenant les valeurs du tableau apiResults pour afficher les données souhaitées
         //Par exemple :
         //Température actuelle :
-        currentTemp = document.querySelector('.temp');
-        currentTemp.textContent = apiResults.current.temp;
+//         currentTemp = document.querySelector('.temp');
+//         currentTemp.textContent = apiResults.current.temp;
 
-        //Température actuelle (arrondie) :
-        currentRoundedTemp = document.querySelector('.roundedTemp');
-        currentRoundedTemp.textContent = Math.round(apiResults.current.temp);
+//         //Température actuelle (arrondie) :
+//         currentRoundedTemp = document.querySelector('.roundedTemp');
+//         currentRoundedTemp.textContent = Math.round(apiResults.current.temp);
 
-        //Température dans 3h (arrondie) :
-        roundedTempInThreeHours = document.querySelector('.threeHoursTemp');
-        roundedTempInThreeHours.textContent = Math.round(apiResults.hourly[3].temp);
+//         //Température dans 3h (arrondie) :
+//         roundedTempInThreeHours = document.querySelector('.threeHoursTemp');
+//         roundedTempInThreeHours.textContent = Math.round(apiResults.hourly[3].temp);
 
-        //Température des 5 prochaines heures :
-        fiveHoursTemp = document.querySelectorAll('.fiveHoursTemp');
-        for(let i = 0; i < fiveHoursTemp.length; i++) {
-            fiveHoursTemp[i].textContent = apiResults.hourly[i + 1].temp;
-        }
+//         //Température des 5 prochaines heures :
+//         fiveHoursTemp = document.querySelectorAll('.fiveHoursTemp');
+//         for(let i = 0; i < fiveHoursTemp.length; i++) {
+//             fiveHoursTemp[i].textContent = apiResults.hourly[i + 1].temp;
+//         }
 
-        //La météo dans 2 jours :
-        weatherInTwoDays = document.querySelector('.weatherInTwoDays');
-        weatherInTwoDays.textContent = apiResults.daily[2].weather[0].description;
+//         //La météo dans 2 jours :
+//         weatherInTwoDays = document.querySelector('.weatherInTwoDays');
+//         weatherInTwoDays.textContent = apiResults.daily[2].weather[0].description;
 
-        weatherInTwoDaysImg = document.querySelector('.weatherInTwoDays-img');
+//         weatherInTwoDaysImg = document.querySelector('.weatherInTwoDays-img');
 
-        //On récupère l'heure actuelle (timestamp * 1000 car JS gère les dates en millisecondes)
-        let time = new Date(apiResults.current.dt * 1000).getHours();
-        if (time >= 6 && time < 21) {
-            weatherInTwoDaysImg.src = `./assets/img/day/${apiResults.daily[2].weather[0].icon}.svg`;
-        } else {
-            weatherInTwoDaysImg.src = `./assets/img/night/${apiResults.daily[2].weather[0].icon}.svg`;
-        }
+//         //On récupère l'heure actuelle (timestamp * 1000 car JS gère les dates en millisecondes)
+//         let time = new Date(apiResults.current.dt * 1000).getHours();
+//         if (time >= 6 && time < 21) {
+//             weatherInTwoDaysImg.src = `./assets/img/day/${apiResults.daily[2].weather[0].icon}.svg`;
+//         } else {
+//             weatherInTwoDaysImg.src = `./assets/img/night/${apiResults.daily[2].weather[0].icon}.svg`;
+//         }
 
 
 
